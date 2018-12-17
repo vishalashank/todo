01c31554c6127e9b4ef412fbc0068dee1ad0392c
vishalashank <vishal.ashank@razorpay.com>
2018-12-17 09:25:00 +0530

    adding factory classes for task and client

--- database/factories/clientFactory.php
+++ database/factories/clientFactory.php
@@ -5,5 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\client::class, function (Faker $faker) {
     return [
         //
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
     ];
 });
--- database/factories/taskFactory.php
+++ database/factories/taskFactory.php
@@ -5,5 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\task::class, function (Faker $faker) {
     return [
         //
+        'client_id' => App\client::pluck('id')->random(),
+        'title' => $faker->sentence,
     ];
 });
