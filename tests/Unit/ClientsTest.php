23e4b5bb958a56c34d4fc20dc89e4f6390bfeb79
vishalashank <vishal.ashank@razorpay.com>
2018-12-17 10:05:40 +0530

    adding unit test for users

---
+++ tests/Unit/ClientsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ClientsTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function testExample()
+    {
+        $this->assertDatabaseHas('clients', ['name' => 'vishal']);
+    }
+}
